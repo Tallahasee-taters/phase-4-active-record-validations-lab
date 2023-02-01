0bef179f6053b37646090de747efb383cd9554e6
Tate <thandley1234@gmail.com>
2023-01-31 17:57:48 -0700

    commit tree ee36a5879211839170aeb6aa07b29e388923ca3a

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: {case_sensitive: true}
+    # validates_presence_of :name <= equal to the validation above
+    # validates :name, uniqueness: true <= uniqueness can be included in line 1
+    validates :phone_number, length: { is: 10 } 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    # Object lifecycle callback
+    # before_validation :capitalize_category_name
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    # validate :true_facts 
+    validates :title, format: {with: /Won't Believe|Secret|Top \d|Guess/i}
+    # def capitalize_category_name
+    #     self.category = self.category.capatilize
+    # end
+
+    # def true_facts
+    #    if [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i].none? {|pattern| pattern.match(title)}
+    #         errors.add(:title, "Not clickbaity enough sir!")
+    #     end 
+    # end
+
 end
